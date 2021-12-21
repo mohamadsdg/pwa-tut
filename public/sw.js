758a3feff8b3945b0816463e62b93bdd9e662b16
Mohamadreza <mohamad.r.sadeghi93@gmail.com>
2021-12-21 10:46:27 +0330

    feat: cache multiple file

--- public/sw.js
+++ public/sw.js
@@ -1,20 +1,35 @@
 self.addEventListener("install", function (event) {
-  console.log("[Service Worker] Installing Service Worker ...", event);
+  console.log("[Service Worker] Installing Service Worker ...");
   event.waitUntil(
     caches.open("static").then(function (cache) {
       console.log("[Service Worker] precaching app shell");
-      cache.add("/src/js/app.js");
+      cache.addAll([
+        "/",
+        "/index.html",
+        "/manifest.json",
+        "/src/js/app.js",
+        "/src/js/feed.js",
+        "/src/js/fetch.js",
+        "/src/js/promise.js",
+        "/src/js/material.min.js",
+        "src/images/main-image.jpg",
+        "/src/css/app.css",
+        "/src/css/feed.css",
+        "https://fonts.googleapis.com/css?family=Roboto:400,700",
+        "https://fonts.googleapis.com/icon?family=Material+Icons",
+        "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css",
+      ]);
     })
   );
 });
 
 self.addEventListener("activate", function (event) {
-  console.log("[Service Worker] Activating Service Worker ....", event);
+  console.log("[Service Worker] Activating Service Worker ....");
   return self.clients.claim();
 });
 
 self.addEventListener("fetch", function (event) {
-  console.log("[Service Worker] Fetching something ....", event);
+  console.log("[Service Worker] Fetching something ....");
 
   return event.respondWith(
     caches.match(event.request).then((response) => {
