87658e8d827a2fd146ffbfb65245605d0f70351c
Mohamadreza <mohamad.r.sadeghi93@gmail.com>
2021-12-21 10:15:18 +0330

    feat: precaching app

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,19 @@
-
-self.addEventListener('install', function(event) {
-  console.log('[Service Worker] Installing Service Worker ...', event);
+self.addEventListener("install", function (event) {
+  console.log("[Service Worker] Installing Service Worker ...", event);
+  event.waitUntil(
+    caches.open("static").then(function (cache) {
+      console.log("[Service Worker] precaching app shell");
+      cache.add("/src/js/app.js");
+    })
+  );
 });
 
-self.addEventListener('activate', function(event) {
-  console.log('[Service Worker] Activating Service Worker ....', event);
+self.addEventListener("activate", function (event) {
+  console.log("[Service Worker] Activating Service Worker ....", event);
   return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event) {
-  console.log('[Service Worker] Fetching something ....', event);
+self.addEventListener("fetch", function (event) {
+  console.log("[Service Worker] Fetching something ....", event);
   event.respondWith(fetch(event.request));
 });
